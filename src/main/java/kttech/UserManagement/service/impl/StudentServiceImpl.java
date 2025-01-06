2d5596280e0a656a7b9b9cc7baced698ebdf8985
Kojo Nhyira Mante-Dankwa <97101203+Kojonhyiraa@users.noreply.github.com>
2025-01-06 21:39:16 +0000

    Update

---
+++ src/main/java/kttech/UserManagement/service/impl/StudentServiceImpl.java
@@ -0,0 +1,61 @@
+package kttech.UserManagement.service.impl;
+
+import kttech.UserManagement.exception.StudentAlreadyExistsException;
+import kttech.UserManagement.model.Student;
+import kttech.UserManagement.repository.StudentRepository;
+import kttech.UserManagement.service.StudentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@RequiredArgsConstructor
+@Service
+public class StudentServiceImpl implements StudentService {
+
+    private final StudentRepository studentRepository;
+
+
+    @Override
+    public Student addStudent(Student student) {
+        if(studentAlreadyExists(student.getEmail())){
+            throw new StudentAlreadyExistsException(student.getEmail()+" already exists");
+        }
+        return studentRepository.save(student);
+    }
+
+
+
+    @Override
+    public List<Student> getAllStudents() {
+        return List.of();
+    }
+
+    @Override
+    public Student getStudentById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Student updateStudent(Student student, Long id) {
+        //Check if user exists and then update values
+        return studentRepository.findById(id).map(st ->{
+            st.setFirstName(student.getFirstName());
+            st.setLastName(student.getLastName());
+            st.setEmail(student.getEmail());
+            st.setDepartment(student.getDepartment());
+            return studentRepository.save(st);
+
+        }).orElseThrow(() -> new )
+    }
+
+    @Override
+    public void deleteStudent(Long id) {
+
+    }
+
+    private boolean studentAlreadyExists(String email) {
+        return studentRepository.findByEmail(email).isPresent();
+    }
+}
