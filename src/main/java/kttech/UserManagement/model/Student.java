5b175de700814cf7c5dcc05e6a81f9a31dc1c49e
Kojo Nhyira Mante-Dankwa <97101203+Kojonhyiraa@users.noreply.github.com>
2025-01-06 21:38:57 +0000

    Update

---
+++ src/main/java/kttech/UserManagement/model/Student.java
@@ -0,0 +1,34 @@
+package kttech.UserManagement.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.annotations.NaturalId;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@RequiredArgsConstructor
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+
+    @NaturalId(mutable = true) //candidate PK. mutable=true allows for modification
+    private String email;
+
+    private String password;
+    private String department;
+
+
+}
