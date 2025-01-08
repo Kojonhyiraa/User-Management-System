111ec57a3718e6caf73ab72e8d97b86d84bcfb4c
Kojo Nhyira Mante-Dankwa <97101203+Kojonhyiraa@users.noreply.github.com>
2025-01-08 07:29:30 +0000

    Created controllers

---
+++ src/main/java/kttech/UserManagement/controller/StudentController.java
@@ -0,0 +1,53 @@
+package kttech.UserManagement.controller;
+
+
+import kttech.UserManagement.model.Student;
+import kttech.UserManagement.service.StudentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/students")
+@RequiredArgsConstructor
+public class StudentController {
+
+    private final StudentService studentService;
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Student>> getAllStudents() {
+        return new ResponseEntity<>(studentService.getAllStudents(),HttpStatus.FOUND);
+    }
+
+    @PostMapping("/add") //Check the Response Entity
+    public ResponseEntity<Student> addStudent(
+            @RequestBody Student student
+            ){
+        return new ResponseEntity<>(studentService.addStudent(student),HttpStatus.CREATED);
+    }
+
+
+    @PatchMapping("/update/{id}")
+    public ResponseEntity<Student> updateStudent(
+            @RequestBody Student student,
+            @PathVariable(name = "id") Long id
+    ){
+        return new ResponseEntity<>(studentService.updateStudent(student,id ),HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Student> getStudentById(
+            @PathVariable(name = "id") Long id) {
+        return new ResponseEntity<>(studentService.getStudentById(id),HttpStatus.OK);
+    }
+
+
+
+
+
+
+
+}
