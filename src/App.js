d15368ebf14faa5c5766c327a0f299ebca75fe9a
Kojo Nhyira Mante-Dankwa <97101203+Kojonhyiraa@users.noreply.github.com>
2025-01-09 23:19:54 +0000

    Update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import Home from "./Home";
 
 function App() {
   return (
     <div className="App">
-   <h2> Welcome</h2>
+   <h2> Welcome to the frontend</h2>
+        <Home />
     </div>
   );
 }
