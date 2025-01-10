502160d36f35c6268200b17b1250cbf5ee0fe1ad
Kojo Nhyira Mante-Dankwa <97101203+Kojonhyiraa@users.noreply.github.com>
2025-01-10 22:38:11 +0000

    Update

--- User-Management-Frontend/src/App.js
+++ User-Management-Frontend/src/App.js
@@ -1,12 +1,11 @@
 
 import './App.css';
-import Home from "./Home";
+import StudentsView from "./component/student/StudentsView";
 
 function App() {
   return (
     <div className="App">
-   <h2> Welcome to the frontend</h2>
-        <Home />
+    <StudentsView/>
     </div>
   );
 }
