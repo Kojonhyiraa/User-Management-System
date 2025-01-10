8ac78e14e7824200068d73572d06547523edcf39
Kojo Nhyira Mante-Dankwa <97101203+Kojonhyiraa@users.noreply.github.com>
2025-01-10 22:38:05 +0000

    Table Init

---
+++ User-Management-Frontend/src/component/student/StudentsView.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const StudentsView = () => {
+    return (
+        <section>
+         <table>
+             <thead>
+             <tr>
+                 <th>ID</th>
+                 <th>First Name</th>
+                 <th>Last Name</th>
+                 <th> Email </th>
+                 <th>Department</th>
+                 <th>Actions</th>
+             </tr>
+             </thead>
+             <tbody>
+
+             </tbody>
+         </table>
+        </section>
+
+    )
+}
+
+export default StudentsView;
