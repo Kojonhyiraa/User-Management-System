0fbaa35cf18a8ea9c889b37105ee3ecbb699f79b
Kojo Nhyira Mante-Dankwa <97101203+Kojonhyiraa@users.noreply.github.com>
2025-01-11 23:51:23 +0000

    Update

--- User-Management-Frontend/src/App.js
+++ User-Management-Frontend/src/App.js
@@ -2,10 +2,13 @@
 import './App.css';
 import StudentsView from "./component/student/StudentsView";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import "/node_modules/bootstrap/dist/js/bootstrap.min.js"
+import NavBar from "./component/common/NavBar";
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
     <StudentsView/>
     </div>
   );
