e4dd6b681c5c0dc9e737b452d15ded284b228c4c
Kojo Nhyira Mante-Dankwa <97101203+Kojonhyiraa@users.noreply.github.com>
2025-01-11 23:51:26 +0000

    Update

--- User-Management-Frontend/src/component/common/NavBar.js
+++ User-Management-Frontend/src/component/common/NavBar.js
@@ -1,20 +1,27 @@
 const NavBar =() => {
 return (
-    <ul className="nav">
-        <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="#">Active</a>
-        </li>
-        <li className="nav-item">
-            <a className="nav-link" href="#">Link</a>
-        </li>
-        <li className="nav-item">
-            <a className="nav-link" href="#">Link</a>
-        </li>
-        <li className="nav-item">
-            <a className="nav-link disabled" aria-disabled="true">Disabled</a>
-        </li>
-    </ul>)
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+            <a className="navbar-brand" href="#">User Manager </a>
 
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li className="nav-item">
+                        <a className="nav-link active" aria-current="page" href="#">View All Students</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Add new student</a>
+                    </li>
+                </ul>
+                </div>
+        </div>
+    </nav>)
 }
 
 export default NavBar;
