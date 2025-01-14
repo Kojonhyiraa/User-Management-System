60a3fa687e153c1796ffcccea74c72577310e618
Kojo Nhyira Mante-Dankwa <97101203+Kojonhyiraa@users.noreply.github.com>
2025-01-14 23:59:03 +0000

    Router dom config

--- User-Management-Frontend/src/App.js
+++ User-Management-Frontend/src/App.js
@@ -4,13 +4,30 @@ import StudentsView from "./component/student/StudentsView";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import "/node_modules/bootstrap/dist/js/bootstrap.min.js"
 import NavBar from "./component/common/NavBar";
-import {BrowserRouter as Router} from "react-router-dom";
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
+import Home from "./Home";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
     <StudentsView/>
+        <Router>
+            <Routes>
+               <Route
+                   exact
+                   path="/"
+                   element={<Home />}></Route>
+
+
+                <Route
+                    exact
+                    path="/"
+                    element={<Home />}></Route>
+            </Routes>
+        </Router>
+
+
     </div>
   );
 }
