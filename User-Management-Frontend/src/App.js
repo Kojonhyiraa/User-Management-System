d3ab232e47b9d58044d9178f0a1e2a12b90be5bc
Kojo Nhyira Mante-Dankwa <97101203+Kojonhyiraa@users.noreply.github.com>
2025-01-14 23:51:52 +0000

    Router dom config

--- User-Management-Frontend/src/App.js
+++ User-Management-Frontend/src/App.js
@@ -4,6 +4,7 @@ import StudentsView from "./component/student/StudentsView";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import "/node_modules/bootstrap/dist/js/bootstrap.min.js"
 import NavBar from "./component/common/NavBar";
+import {BrowserRouter as Router} from "react-router-dom";
 
 function App() {
   return (
