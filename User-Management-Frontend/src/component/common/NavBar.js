6366b9af5e9df2e51a0b5500f21b04d1aa786079
Kojo Nhyira Mante-Dankwa <97101203+Kojonhyiraa@users.noreply.github.com>
2025-01-14 23:44:19 +0000

    Update

--- User-Management-Frontend/src/component/common/NavBar.js
+++ User-Management-Frontend/src/component/common/NavBar.js
@@ -1,8 +1,11 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+
 const NavBar =() => {
 return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
-            <a className="navbar-brand" href="#">User Manager </a>
+            <Link className="navbar-brand" to={"/"}>User Manager </Link>
 
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
