9851c132a34ce46c6ec376142e1460f2daeb31b1
Kojo Nhyira Mante-Dankwa <97101203+Kojonhyiraa@users.noreply.github.com>
2025-01-14 23:50:36 +0000

    Router dom config

--- User-Management-Frontend/src/component/common/NavBar.js
+++ User-Management-Frontend/src/component/common/NavBar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Link} from "react-router-dom";
 
 const NavBar =() => {
-return (
+   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
             <Link className="navbar-brand" to={"/"}>User Manager </Link>
@@ -16,10 +16,10 @@ return (
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                     <li className="nav-item">
-                        <a className="nav-link active" aria-current="page" href="#">View All Students</a>
+                        <Link className="nav-link active" aria-current="page" to={"view-students"}>View All Students</Link>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#">Add new student</a>
+                        <Link className="nav-link" to={"/add-students"}>Add new student</Link>
                     </li>
                 </ul>
                 </div>
