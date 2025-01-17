bb68438660cdc55d1ea12f8bd9826a25f7acdca4
Kojo Nhyira Mante-Dankwa <97101203+Kojonhyiraa@users.noreply.github.com>
2025-01-17 16:53:18 +0000

    Update

--- User-Management-Frontend/src/component/common/NavBar.js
+++ User-Management-Frontend/src/component/common/NavBar.js
@@ -3,28 +3,30 @@ import {Link} from "react-router-dom";
 
 const NavBar =() => {
    return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
-        <div className="container-fluid">
-            <Link className="navbar-brand" to={"/"}>User Management System </Link>
+       <nav className="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
+           <div className="container-fluid">
+               <Link className="navbar-brand" to={"/"}>User Management System </Link>
 
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
+               <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                       data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                       aria-expanded="false" aria-label="Toggle navigation">
+                   <span className="navbar-toggler-icon"></span>
+               </button>
 
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li className="nav-item">
-                        <Link className="nav-link active" aria-current="page" to={"view-students"}>View All Students</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to={"/add-students"}>Add new student</Link>
-                    </li>
-                </ul>
-                </div>
-        </div>
-    </nav>)
+               <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                   <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                       <li className="nav-item">
+                           <Link className="nav-link active" aria-current="page" to={"view-students"}>View All
+                               Students</Link>
+                       </li>
+                       <li className="nav-item">
+                           <Link className="nav-link" to={"/add-students"}>Add new student</Link>
+                       </li>
+                   </ul>
+               </div>
+           </div>
+
+       </nav>)
 }
 
 export default NavBar;
