7e9c5197606ab7e50bf821d648e1c0935f8f36a0
Kojo Nhyira Mante-Dankwa <97101203+Kojonhyiraa@users.noreply.github.com>
2025-01-18 14:18:23 +0000

    Update

--- User-Management-Frontend/src/App.js
+++ User-Management-Frontend/src/App.js
@@ -7,6 +7,7 @@ import "/node_modules/bootstrap/dist/js/bootstrap.min.js"
 import NavBar from "./component/common/NavBar";
 import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
 import Home from "./Home";
+import EditStudent from "./component/student/EditStudent";
 
 function App() {
   return (
@@ -29,6 +30,11 @@ function App() {
                     exact
                     path="/add-students"
                     element={<CreateStudent />}></Route>
+
+                <Route
+                    exact
+                    path="/update-students"
+                    element={<EditStudent />}></Route>
             </Routes>
         </Router>
 
