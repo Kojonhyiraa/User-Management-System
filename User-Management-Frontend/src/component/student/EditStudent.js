d2b46a92433052bb6bfa8276caa7750ece11e9cb
Kojo Nhyira Mante-Dankwa <97101203+Kojonhyiraa@users.noreply.github.com>
2025-01-19 18:46:56 +0000

    Update

--- User-Management-Frontend/src/component/student/EditStudent.js
+++ User-Management-Frontend/src/component/student/EditStudent.js
@@ -1,9 +1,11 @@
-import React, {useState} from 'react';
-import {Link, useNavigate} from "react-router-dom";
+import React, {useEffect, useState} from 'react';
+import {Link, useNavigate, useParams} from "react-router-dom";
 import axios from "axios";
 
 const EditStudent = () => {
         const navigate = useNavigate();
+
+        const {id} = useParams();
 
     const [student, setStudent] = useState({
         firstName: "",
@@ -11,6 +13,25 @@ const EditStudent = () => {
         email: "",
         department: ""
     });
+
+
+
+
+    useEffect(() => {
+        getOneStudent();
+    },[])
+
+    const getOneStudent = async() => {
+        const response = await axios.get(`http://localhost:8080/students/${id}`);
+            setStudent(response.data);
+    }
+
+
+
+
+
+
+
     const [errorMessage, setErrorMessage] = useState(""); // Error message state
     const [isPopupVisible, setIsPopupVisible] = useState(false); // Popup visibility state
 
@@ -20,31 +41,25 @@ const EditStudent = () => {
         setStudent({ ...student, [e.target.name]: e.target.value });
     };
 
-    const createStudent = async (e) => {
+    const updateStudent = async (e) => {
         e.preventDefault();
         try {
-            const response = await axios.post("http://localhost:8080/students/add", student);
+            const response = await axios.patch(`http://localhost:8080/students/update/${id}`, student);
 
-            if (response.status === 201) {
+            if (response.status === 200) {
                 setIsPopupVisible(true); // Show popup on success
             }
         } catch (error) {
             if (error.response && error.response.data && error.response.data.error) {
                 setErrorMessage(error.response.data.error);
             } else {
-                setErrorMessage("There was an error creating the student.");
+                setErrorMessage("There was an error updating the student.");
             }
         }
     };
 
     const handlePopupClose = () => {
         setIsPopupVisible(false);
-        setStudent({
-            firstName: "",
-            lastName: "",
-            email: "",
-            department: ""
-        }); // Reset the student inputs
     };
 
     const handleViewAllStudents = () => {
@@ -55,13 +70,13 @@ const EditStudent = () => {
     return (
         <div className="background">
             <div className="form-container">
-                <h2 className="form-title">Create New Student</h2>
+                <h2 className="form-title">Update Student</h2>
 
                 {errorMessage && (
                     <div className="alert alert-danger fade-in">{errorMessage}</div>
                 )}
 
-                <form onSubmit={createStudent} className="student-form">
+                <form onSubmit={updateStudent} className="student-form">
                     <div className="form-group">
                         <label htmlFor="firstName"><b>First Name</b></label>
                         <input
@@ -119,7 +134,7 @@ const EditStudent = () => {
                     </div>
 
                     <div className="button-group">
-                        <button type="submit" className="btn btn-success">Save</button>
+                        <button type="submit" className="btn btn-success">Update</button>
                         <Link to="/view-students" className="btn btn-danger">Cancel</Link>
                     </div>
                 </form>
@@ -128,8 +143,8 @@ const EditStudent = () => {
             {isPopupVisible && (
                 <div className="popup-overlay">
                     <div className="popup-content">
-                        <h3>Student Created Successfully!</h3>
-                        <p>The student has been added to the database.</p>
+                        <h3>Update Successful</h3>
+                        <p>The student has been updated successfully.</p>
                         <div className="popup-buttons">
                             <button
                                 className="btn btn-danger"
